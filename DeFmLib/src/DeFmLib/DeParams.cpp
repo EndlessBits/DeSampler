e6d170caa9545c6d95d9b39b7c1561d800f9599a
perevalovds <perevalovds@gmail.com>
2022-09-17 11:18:19 +0300

    ramp to freq and amp

--- DeFmLib/src/DeFmLib/DeParams.cpp
+++ DeFmLib/src/DeFmLib/DeParams.cpp
@@ -6,27 +6,45 @@ DeParams::DeParams(float midi_note0, float midi_note1, float vol0, float vol1)
 {
 	this->midi_note0 = midi_note0;
 	this->midi_note1 = midi_note1;
-	midi_delta_ = this->midi_note1 - this->midi_note0;
+	midi_delta_ = midi_note1 - midi_note0;
 
-	this->vol0 = vol0;
-	this->vol1 = vol1;
+	this->db0 = db0;
+	this->db1 = db1;
+	db_delta_ = db1 - db0;
+	float amp0 = db_to_amp_raw(db0);	
+	float amp1 = db_to_amp_raw(db1);
+	db_k_ = 1.0/(amp1-amp0);			// mapping v in [amp0, amp1] -> k*v + a in [0,1]
+	db_a_ = -db_k_ * amp0;
 }
 
 //--------------------------------------------------------------
+// https://hydrogenaud.io/index.php?topic=86414.0
+// db = 20*log10(amplitude)
+//      6.02 * log2(Amplitude)
+//amplitude = pow(2, dB/6.02), or pow(10, dB/20)
+
+inline float DeParams::db_to_amp_raw(float db) {
+	return pow(10.0f, db / 20.0f);
+}
+
+//--------------------------------------------------------------
+//0..1->0..1
 float DeParams::ramp_to_volume(float x)
 {
-	return ofMap(exp(x),exp(0),exp(1),vol0, vol1);	//vol0..vol exponentially  // TODO table
+	x = x * db_delta_ + db0;
+	return db_k_ * db_to_amp_raw(x) + db_a_;		
 }
 
 //--------------------------------------------------------------
 // https://newt.phys.unsw.edu.au/jw/notes.html
 // 69 is A, 440Hz, m=0..127
-//m  =  12*log2(fm/440 Hz) + 69     and    fm  =  2^(m−69)/12(440 Hz).
+// m = 12*log2(fm/440 Hz) + 69 and fm = 2^((m-69)/12) * (440 Hz).
 
+//0..1->Hz
 float DeParams::ramp_to_freq(float x)
 {
 	x = x * midi_delta_ + midi_note0;
-	return pow(2, x − 69) / 12;
+	return pow(2, (x - 69.0f) / 12.0f) * 440.0f;
 }
 
 //--------------------------------------------------------------
--- DeFmLib/src/DeFmLib/DeParams.h
+++ DeFmLib/src/DeFmLib/DeParams.h
@@ -7,23 +7,26 @@
 class DeParams {
 public:
 	DeParams() {}
-	DeParams(float midi_note0, float midi_note1, float vol0, float vol1);
+	DeParams(float midi_note0, float midi_note1, float db0, float db1);
 
-	// Parameters
+	// Ranges
 	float midi_note0 = 21;
 	float midi_note1 = 108;
-	float vol0 = 0;
-	float vol1 = 1;
-	int wavetable_lenth = 1024;
+	float db0 = -40;
+	float db1 = 0;
 
 	// Conversions
-	float ramp_to_volume(float x);	// Convert 0..1 to exp-scale volume
-	float ramp_to_freq(float x);	// Convert 0..1 to exp-scale freq
-	float volume_to_ramp(float x);	
+	float ramp_to_volume(float x);	//0..1->0..1
+	float ramp_to_freq(float x);	//0..1->Hz
+	float volume_to_ramp(float x);
 	float freq_to_ramp(float x);
 
 	// Triangle wavetable
 	float wavetable(float phase);
 protected:
 	float midi_delta_ = 0;
+	float db_delta_ = 0;
+	float db_k_ = 1;
+	float db_a_ = 0;
+	inline float db_to_amp_raw(float db);
 };

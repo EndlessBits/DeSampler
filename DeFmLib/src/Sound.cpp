b0c150de9b49c96d8a951cc73d8402f5d184ba8c
perevalovds <perevalovds@gmail.com>
2022-09-29 23:10:09 +0300

    sound out (not work)

--- DeFmLib/src/Sound.cpp
+++ DeFmLib/src/Sound.cpp
@@ -17,10 +17,12 @@ void Sound::setup() {
 	//auto devices = sound_stream_.getMatchingDevices("default");
 
 
+	stream_sr_ = 22050;
+
 	int buffer_size = 128;
 
 	settings.setOutListener(this);
-	settings.sampleRate = 44100;
+	settings.sampleRate = stream_sr_;
 	settings.numOutputChannels = 2;
 	settings.numInputChannels = 2;
 	settings.bufferSize = buffer_size;
@@ -35,7 +37,20 @@ void Sound::exit() {
 
 //--------------------------------------------------------------
 void Sound::audioOut(ofSoundBuffer& output) {
-	//INSTR.at_update();
+	if (playing_) {
+		const float volume = 0.6f;
+		for (int i = 0; i < output.getNumFrames(); i++) {
+			float value = (play_pos_++ < mono_sound_.size()) ? mono_sound_[i] : 0;
+			value *= volume;
+			output.getSample(i, 0) = value;
+			output.getSample(i, 1) = value;
+
+		}
+		if (play_pos_ >= mono_sound_.size()) {
+			play_pos_ = int(mono_sound_.size()) - 1;
+			playing_ = 0;
+		}
+	}
 }
 
 //--------------------------------------------------------------
@@ -44,7 +59,7 @@ void Sound::audioIn(ofSoundBuffer& input) {
 }
 
 //--------------------------------------------------------------
-void Sound::update(){
+void Sound::update() {
 
 
 }
@@ -56,19 +71,34 @@ void Sound::draw(const ofRectangle& rect_pix) {
 	ofNoFill();
 	ofDrawRectangle(rect_pix);
 
-	ofSetColor(255);
+	int n = mono_sound_.size();
+	if (n > 0) {
+		float x0 = rect_pix.x;
+		float y0 = rect_pix.y;
+		int w = rect_pix.width;
+		float h2 = rect_pix.height / 2;
 
+		ofSetColor(255);
+		for (int i = 0; i < w; i++) {
+			float v = mono_sound_[(i * n) / w] * h2;
+			ofDrawLine(x0 + i, y0 + h2, x0 + i, y0 + h2 - v);
+		}
+	}
 
 }
 
 //--------------------------------------------------------------
 void Sound::set_sound(vector<float>& mono_sound, int sample_rate) {
-
+	mono_sound_ = mono_sound;
+	sound_sr_ = sample_rate;
 }
 
 //--------------------------------------------------------------
 void Sound::play_sound() {
-
+	if (!mono_sound_.empty()) {
+		play_pos_ = 0;
+		playing_ = 1;
+	}
 }
 
 //--------------------------------------------------------------
--- DeFmLib/src/Sound.h
+++ DeFmLib/src/Sound.h
@@ -19,7 +19,13 @@ public:
 	void audioIn(ofSoundBuffer& input);
 
 protected:
+	int stream_sr_ = 0;
 	ofSoundStream sound_stream_;
+
+	vector<float> mono_sound_;		// TODO change using mutex
+	int sound_sr_ = 0;
+	int playing_ = 0;
+	int play_pos_ = 0;
 
 };
 

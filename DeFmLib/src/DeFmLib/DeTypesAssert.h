69bd2e564a935218b0f22a62cac13f6d7a359ced
perevalovds <perevalovds@gmail.com>
2022-10-06 16:55:11 +0300

    debug break

--- DeFmLib/src/DeFmLib/DeTypesAssert.h
+++ DeFmLib/src/DeFmLib/DeTypesAssert.h
@@ -6,5 +6,8 @@ typedef unsigned char uint8;
 typedef char int8;
 
 //Exceptions
-#define de_exception(message) { cout << message << endl; ofSystemAlertDialog(message); _Exit(1); }
+#define de_exception(message) { cout << message << endl; __debugbreak(); ofSystemAlertDialog(message); _Exit(1); }
 #define de_assert(cond, message) {if (!(cond)) de_exception(message);}
+
+//OutputDebugStringA( message "\n" ); 
+//		*(volatile int*)nullptr = 1;        
